8c8655a5727e17520b54180b9c7bd40535a138be
Farhan Kassam <farhankassam2@gmail.com>
2019-04-27 16:58:03 -0500

    updated signup script

--- EarthxHackathon2019/script.js
+++ EarthxHackathon2019/script.js
@@ -1,7 +1,32 @@
-const Http = new XMLHttpRequest();
-const url='localhost:5000/user/111';
-Http.open("GET", url);
-Http.send();
-Http.onreadystatechange=(e)=>{
-console.log(Http.responseText)
-}
+// const Http = new XMLHttpRequest();
+// const url='localhost:5000/user/111';
+// Http.open("GET", url);
+// Http.send();
+// Http.onreadystatechange=(e)=>{
+// console.log(Http.responseText)
+// }
+
+let userSignup = function (password, email, numberOfPeople, gallonsUsed, threshold) {
+  return new Promise(function (fulfill, reject) {
+  const xhttp = new XMLHttpRequest();
+  const json = {
+    "password": password,
+    "email": email,
+    "numberOfPeople": numberOfPeople,
+    "gallonsUsed": gallonsUsed,
+    "threshold": threshold
+  };
+  xhttp.open("POST", "/user/" + email, true);
+  xhttp.send(json);
+  xhttp.onload = function (request, event) {
+            console.log(request, event);
+            console.log(xhttp.responseText);
+            let response = xhttp.responseText;
+            fulfill(response);
+        };
+
+        xhttp.onerror = function (request, event) {
+            console.log(request, event);
+            reject ("request not processed succesfully");
+        };
+});
