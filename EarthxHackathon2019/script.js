e8069b00ecdd467c6aa3ed1a38fb7973fdf88abd
Nishant Bhagat <bobbane1011@gmail.com>
2019-04-27 21:45:56 -0500

    Finished Login and Sign Up Functionality

--- EarthxHackathon2019/script.js
+++ EarthxHackathon2019/script.js
@@ -6,18 +6,22 @@
 // console.log(Http.responseText)
 // }
 
-let userSignup = function (password, email, numberOfPeople, gallonsUsed, threshold) {
+let userSignup = function (password, email, numberOfPeople, username) {
   return new Promise(function (fulfill, reject) {
   const xhttp = new XMLHttpRequest();
   const json = {
     "password": password,
     "email": email,
-    "numberOfPeople": numberOfPeople,
-    "gallonsUsed": gallonsUsed,
-    "threshold": threshold
+    "username": username,
+    "numberOfPeople": parseInt(numberOfPeople),
+    "gallonsUsed": 0,
+    "threshold": 10
   };
-  xhttp.open("POST", "/user/" + email, true);
-  xhttp.send(json);
+  url = "http://localhost:5000/user/" + username;
+//   '?password=' + password + '&numberOfPeople=' + numberOfPeople
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.send(JSON.stringify(json));
   xhttp.onload = function (request, event) {
             console.log(request, event);
             console.log(xhttp.responseText);
@@ -30,3 +34,30 @@ let userSignup = function (password, email, numberOfPeople, gallonsUsed, thresho
             reject ("request not processed succesfully");
         };
 });}
+
+let userLogin = function(username, password) {
+
+    return new Promise(function (fulfill, reject) {
+        const xhttp = new XMLHttpRequest();
+        url = "http://localhost:5000/user/" + password + username;
+        xhttp.open("GET", url, true);
+        xhttp.setRequestHeader('Content-Type', 'application/json');
+        xhttp.send();
+        xhttp.onload = function (request, event) {
+                  console.log(request, event);
+                  console.log(xhttp.responseText);
+                  let response = xhttp.responseText;
+                  fulfill(response);
+              };
+      
+              xhttp.onerror = function (request, event) {
+                  console.log(request, event);
+                  reject ("request not processed succesfully");
+              };
+      });
+    
+}
+
+document.getElementById("login").onclick = function () {
+    const UserInfo = userLogin(document.getElementsByName('login_usr')[0].value, document.getElementsByName('login_pwd')[0].value);
+};
